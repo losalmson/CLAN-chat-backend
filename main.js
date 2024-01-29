1aae19b6391168263631f0c7058ea900fe7ae6a1
losalmson <mialo.salmson@gmail.com>
2024-01-29 13:59:02 +0100

    Added test messages & REST testing Co-authored-by: Carlos Johansson Bergqvist <Cralcas@users.noreply.github.com> Co-authored-by: nickenickenicke <nickenickenicke@users.noreply.github.com> Co-authored-by: Andreas Retzius <retzerbil@users.noreply.github.com>

--- main.js
+++ main.js
@@ -7,9 +7,42 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Funkar");
-})
+const messages = [
+    {
+        user: "Nalle Puh",
+        message: "Ge mig honung bitch",
+        messageId: 1
+    },
+    {
+        user: "Tiger",
+        message: "Känner mig fett studsig idag",
+        messageId: 2
+    },
+    {
+        user: "Ior",
+        message: "Jag hatar mitt liv",
+        messageId: 3
+    },
+    {
+        user: "Batman",
+        message: "I'm batman",
+        messageId: 4
+    },
+    {
+        user: "Nasse",
+        message: "Hej hej",
+        messageId: 5
+    },
+    {
+        user: "Christoffer Robin",
+        message: "Har någon sett min ballong?",
+        messageId: 6
+    },
+];
+
+app.get("/api/messages", (req, res) => {
+    res.send(messages);
+});
 
 app.listen(port, () =>{
     console.log("Listening to port " + port);
