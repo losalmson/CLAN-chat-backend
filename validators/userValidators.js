4d267c7d94f6ecc6f5073c8b4adc07511b3e283e
Cralcas <carlosbergq@gmail.com>
2024-02-21 15:05:15 +0100

    userValidation console.log
    Co-authored-by: losalmson <losalmson@users.noreply.github.com>
    Co-authored-by: Andreas Retzius <retzerbil@users.noreply.github.com>
    Co-authored-by: nickenickenicke <nickenickenicke@users.noreply.github.com>

--- validators/userValidators.js
+++ validators/userValidators.js
@@ -63,8 +63,10 @@ const validateLoginUserAccount = [
 
   (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty())
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
       return res.status(422).json({errors: errors.array()});
+    }
     next();
   },
 ];
